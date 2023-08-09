77688d62ec506040c1819f3e69e9309b55e3c27c
ADITYA CHOUDHARY <108794914+Adityach007@users.noreply.github.com>
2023-08-09 23:47:32 +0530

    Add files via upload

---
+++ Face_Recognition_OpenCV/AccessTo_webcam.py
@@ -0,0 +1,23 @@
+import cv2
+from cvzone.FaceDetectionModule import FaceDetector
+from cvzone.SerialModule import SerialObject
+
+cap = cv2.VideoCapture(0)
+detector = FaceDetector()
+arduino = SerialObject('COM7')
+
+while True:
+    success, img = cap.read()
+    img, bBoxes = detector.findFaces(img)
+
+    if bBoxes:
+        arduino.sendData([1, 0])
+    else:
+        arduino.sendData([0, 1])
+
+
+    cv2.imshow("Video", img)
+    cv2.waitKey(1)
+
+
+
